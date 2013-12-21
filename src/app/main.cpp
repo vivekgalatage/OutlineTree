9fee719fac9d408750de9e9be989056c62d548ac
Vivek Galatage <vivekgalatage@gmail.com>
2013-12-21 10:16:19 +0530

    Show usage information on invalid arguments.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,9 +4,15 @@
 using namespace otree;
 
 #define USAGE_INFORMATION \
+"\n" \
 "otree: Displays outline information for the C++ source code.\n" \
 "Usage:\n" \
 "    -h, --help: Dispalys this message.\n" \
+
+inline void displayUsageInformation()
+{
+    INFO(USAGE_INFORMATION);
+}
 
 int main(int argc, char* argv[])
 {
@@ -16,12 +22,13 @@ int main(int argc, char* argv[])
         ERROR("Invalid argument" << (error.arguments.size() > 1 ? "s" : "")) ;
         for (unsigned i = 0; i < error.arguments.size(); ++i)
             ERROR("    " << error.arguments[i]);
+        displayUsageInformation();
         return -1;
     }
 
     CommandLine& cmdLine = CommandLine::current();
     if (cmdLine.hasArgument("help") || cmdLine.hasArgument("h")) {
-        INFO(USAGE_INFORMATION);
+        displayUsageInformation();
         return 0;
     }
 
